3c1ed7e5dabd6e8ee8f5d838ed6c807eccc36fc7
Melisa Rivas <rivasmelisa2024@gmail.com>
2024-09-30 07:58:31 -0600

    revisando clase del jueves

--- Clase01/26_sept.py
+++ Clase01/26_sept.py
@@ -10,7 +10,10 @@ meta = 2000.00
 
 texto_completo = nombre_completo + " tiene " + str(edad) + " años, "
 texto_completo_f = texto_completo + "y su meta es... " + str(meta)
-texto_formateado = f"La meta es... ${meta:,.2f}"
+texto_formateado = f"La meta es... ${meta:,.2f}" 
+#El f al inicio es para que se pueda trabajar mejor con un string, son como las backticks en JS
+# Al final de meta se ocupa 2f para darle dos decimales, en caso que quisiera mas decimales, pongo
+# numero_f
 
 print(nombre)
 print(apellido)
