65d15e384d677ff85472f6fd86329f5b1315aed8
Melisa Rivas <rivasmelisa2024@gmail.com>
2024-10-23 17:20:45 -0600

    23-10-2024

--- clase05/listasDeListas.py
+++ clase05/listasDeListas.py
@@ -1,7 +1,9 @@
+#\t es tabulacion, \n es salto de linea
 
 online = True
 person = []
-people = []
+people = ["Juan", 'Maria', 'Sebastian', 'Marta']
+ages = [23, 21, 20, 19]
 
 # Las triples comillas sirve para escribir textos de mas de una linea
 menu = """ Elija una de las siguiente opciones
@@ -9,11 +11,14 @@ menu = """ Elija una de las siguiente opciones
         2. Crear people                
         3. Salir"""
 
-# while online: 
-#     print(menu)
-#     opcion = int(input('\tOpcion: '))
-#     if opcion == 3:
-#         online = False
-#     elif opcion == 1:
-#         if len(people) > 0:
-#             for n in range(len(people)):
+while online: 
+    print(menu)
+    opcion = int(input('\tOpcion: '))
+    if opcion == 3:
+        online = False
+    elif opcion == 1:
+        if len(people) > 0:
+            for n in range(len(people)):
+                print(str(n+1)+ ". " + people[n]+ " - " + str(ages[n]))
+        else: 
+            print('No hay registros')
